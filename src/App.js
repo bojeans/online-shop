6754392cf75d9f4acd0f4da5be45508df16a6de0
Chris Anderson <chrisanderson@developers.institute.co.nz>
2022-04-09 21:17:17 +1200

    imported fetch product code into app.js as well as setting up state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { React, useEffect, useState } from "react";
 import "./App.css";
+import { fetchProducts } from "./services/fetchProducts"
 
 function App() {
-  return <div>hello world</div>;
+  const [products, setProducts] = useState([]);
+
+  const loadData = async () => {
+    const products = await fetchProducts();
+    setProducts(products);
+
+   
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+  <div>
+    hello world
+    {products}
+    </div>
+    );
 }
 
 export default App;
